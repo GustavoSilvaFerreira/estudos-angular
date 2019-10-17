35c498e836019cef0b50911db5f65ac43da74057
Gustavo da Ferreira <gustavo.da.ferreira@avanade.com>
2019-10-17 11:36:30 -0300

    remover import de picture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { PictureInstagranComponent } from './picture-instagran/picture-instagran.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -10,7 +9,6 @@ import { LoginComponent } from './security/login/login.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'picture', component: PictureInstagranComponent},
   {path: 'login', component: LoginComponent},
   {path: 'todo-list', loadChildren: () => import('./todo/todo.module').then(m => m.TodoModule),
                       canActivate: [LoggedinGuard],
